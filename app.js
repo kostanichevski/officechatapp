7a75d7aab3753e3e395fa0826145b85d6b65d710
Kosta <kostanicevski@gmail.com>
2023-08-21 19:56:47 +0200

    fifth

--- app.js
+++ app.js
@@ -14,8 +14,31 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/oca/login", authHandler.login);
-app.get("/oca/login");
+app.get("/oca/signup", authHandler.signup);
 db.init();
+
+app.use(
+  jwt
+    .expressjwt({
+      algorithms: ["HS256"],
+      secret: process.env.JWT_SECRET,
+      getToken: (req) => {
+        if (
+          req.headers.authorization &&
+          req.headers.authorization.split(" ")[0] === "Bearer"
+        ) {
+          return req.headers.authorization.split(" ")[1];
+        }
+        if (req.cookies.jwt) {
+          return req.cookies.jwt;
+        }
+        return null;
+      },
+    })
+    .unless({
+      path: ["/oca/signup", "/oca/login"],
+    })
+);
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
